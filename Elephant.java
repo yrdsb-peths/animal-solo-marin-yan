8f7b8bb5c0fd8fc8f8c93cef31769015019902b2
marin-yan <marinxyan@gmail.com>
2025-04-29 08:14:45 -0400

    moved elephant

--- Elephant.java
+++ Elephant.java
@@ -8,12 +8,11 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Elephant extends Actor
 {
-    /**
-     * Act - do whatever the Elephant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    public Elephant() {
+        setImage("images/elephant.png");  
+    }
     public void act()
     {
-        // Add your action code here.
+        move(1);
     }
 }
