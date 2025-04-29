ba75ddc2b62826283f45cbe67b61179242f10fad
marin-yan <marinxyan@gmail.com>
2025-04-29 09:13:47 -0400

    added background image

--- Apple.java
+++ Apple.java
@@ -12,6 +12,9 @@ public class Apple extends Actor
      * Act - do whatever the Apple wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Apple() {
+        setImage("images/apple1.png");  
+    }
     public void act()
     {
         // Apple falls downwards
