036bd38689d92b3047ae4b4e5e9129aaa1741e5e
marin-yan <marinxyan@gmail.com>
2025-04-29 08:48:54 -0400

    made a score tracker

--- MyWorld.java
+++ MyWorld.java
@@ -1,14 +1,33 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    
+    public int score = 0;
+    Label scoreLabel;
+    
     public MyWorld() {
         super(600, 400, 1);
         
+        // Create the elephant object
+        
         Elephant elephant = new Elephant();
-        addObject(elephant,300, 200);
+        addObject(elephant,300, 300);
+        
+        // Create a label
+        scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 50, 50);
         
         createApple();
     }
+    /**
+     * Increase score
+     */
+    public void increaseScore() {
+        score++;
+        scoreLabel.setValue(score);
+    }
+    
+    
     /**
      * Create a new apple at random location at top of screen
      */
