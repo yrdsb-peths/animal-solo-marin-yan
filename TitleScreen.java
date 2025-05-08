a41d7e88ce6f8a96b141923c1a92cabdd04db028
marin-yan <marinxyan@gmail.com>
2025-05-08 09:05:42 -0400

    updated the title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class TitleScreen extends World
 {
-    Label titleLabel = new Label("Hungry Elepahnt", 60);
+    Label titleLabel = new Label("Hungry Elephant", 60);
     /**
      * Constructor for objects of class TitleScreen.
      * 
