81167bf1ffc82a23ff8c0cc0d15b826f9007a381
marin-yan <marinxyan@gmail.com>
2025-05-09 08:41:50 -0400

    updated the background image

--- TitleScreen.java
+++ TitleScreen.java
@@ -16,8 +16,7 @@ public class TitleScreen extends World
 
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        GreenfootImage bg = new GreenfootImage("images/background1.png");
-        getBackground().drawImage(bg, 0, 0);
+        
 
         addObject(titleLabel, getWidth()/2, 200);
         prepare();
